b3cc7114023635266f31750a19dec79a45d96557
PhanPhuongPhi <phanphuongphi@gmail.com>
2025-07-15 10:59:23 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Kết nối DB
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Dịch vụ MVC
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Middleware
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
